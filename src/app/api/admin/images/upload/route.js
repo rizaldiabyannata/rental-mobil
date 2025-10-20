b6ad2ecea39a61ed93269bf2b09e382fff92c686
darafinaselen <darafielena@gmail.com>
2025-10-21 03:16:39 +0800

    fix: images/route.js

--- src/app/api/admin/images/upload/route.js
+++ src/app/api/admin/images/upload/route.js
@@ -1,6 +1,7 @@
 import { NextResponse } from "next/server";
-import path from "path";
-import { writeFile } from "fs/promises";
+import { minioClient } from "@/lib/minio";
+// import path from "path";
+// import { writeFile } from "fs/promises";
 
 export async function POST(request) {
   try {
@@ -14,28 +15,48 @@ export async function POST(request) {
       );
     }
 
+    const bucketName = process.env.MINIO_BUCKET;
     const uploadedUrls = [];
+
+    const bucketExists = await minioClient.bucketExists(bucketName);
+    if (!bucketExists) {
+      await minioClient.makeBucket(bucketName, "us-east-1");
+    }
 
     for (const file of files) {
       const buffer = Buffer.from(await file.arrayBuffer());
       const filename = Date.now() + "_" + file.name.replaceAll(" ", "_");
-      const uploadPath = path.join(
-        process.cwd(),
-        "public/uploads/tours",
-        filename
-      );
+      // const uploadPath = path.join(
+      //   process.cwd(),
+      //   "public/uploads/tours",
+      //   filename
+      // );
+      await minioClient.putObject(bucketName, filename, buffer, file.size);
 
-      await writeFile(uploadPath, buffer);
-      const publicUrl = `/uploads/tours/${filename}`;
+      // await writeFile(uploadPath, buffer);
+      // const publicUrl = `/uploads/tours/${filename}`;
+      // uploadedUrls.push(publicUrl);
+      const publicUrl = `${
+        process.env.MINIO_USE_SSL === "true" ? "https" : "http"
+      }://${process.env.MINIO_ENDPOINT}:${
+        process.env.MINIO_PORT
+      }/${bucketName}/${filename}`;
       uploadedUrls.push(publicUrl);
     }
 
+    // return NextResponse.json({
+    //   message: "Upload berhasil!",
+    //   urls: uploadedUrls,
+    // });
     return NextResponse.json({
-      message: "Upload berhasil!",
+      message: "Upload ke MinIO berhasil!",
       urls: uploadedUrls,
     });
   } catch (error) {
-    console.error("Upload error:", error);
-    return NextResponse.json({ error: "Upload gagal." }, { status: 500 });
+    console.error("MinIO Upload error:", error);
+    return NextResponse.json(
+      { error: "Upload ke MinIO gagal." },
+      { status: 500 }
+    );
   }
 }
